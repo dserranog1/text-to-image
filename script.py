8f66f40fe08660a4792e8efdf4435063ba245765
Daniel Serrano <dserranog@unal.edu.co>
2023-09-24 20:06:27 -0500

    feat: pixels can now be replicated to make them more visible

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from math import sqrt
 import random
 
 
@@ -7,40 +8,48 @@ def text_to_binary(text) -> str:
     return binary_data
 
 
-def create_image_with_random_fill(binary_data, image_width, image_height):
+def create_image_with_random_fill(binary_data, area_per_pixel):
+    if area_per_pixel == 0:
+        width_and_height_step = 1
+    else:
+        width_and_height_step = area_per_pixel
     while len(binary_data) % 24 != 0:
         binary_data = binary_data + str(random.randint(0, 1))
 
     pixels = [binary_data[i : i + 24] for i in range(0, len(binary_data), 24)]
 
+    number_of_pixels = len(pixels)
+    image_width = int(sqrt(number_of_pixels))
+    image_height = image_width
     image = Image.new("RGB", (image_width, image_height))
 
     pixel_index = 0
 
-    for y in range(image_height):
-        for x in range(image_width):
-            if pixel_index < len(pixels):
-                r = int(pixels[pixel_index][:8], 2)
-                g = int(pixels[pixel_index][8:16], 2)
-                b = int(pixels[pixel_index][16:], 2)
-                pixel_index += 1
-            else:
-                r = random.randint(254, 255)
-                g = random.randint(254, 255)
-                b = random.randint(254, 255)
+    for y in range(0, image_height, width_and_height_step):
+        for x in range(0, image_width, width_and_height_step):
+            if pixel_index >= number_of_pixels:
+                return image
+            r = int(pixels[pixel_index][:8], 2)
+            g = int(pixels[pixel_index][8:16], 2)
+            b = int(pixels[pixel_index][16:], 2)
+            pixel_index += 1
             image.putpixel((x, y), (r, g, b))
-
+            for i in range(0, area_per_pixel):
+                if x + i >= image_width:
+                    continue
+                for j in range(0, area_per_pixel):
+                    if y + j >= image_height:
+                        continue
+                    image.putpixel((x + i, y + j), (r, g, b))
     return image
 
 
-def text_to_image(width=700, height=500):
+def text_to_image():
     with open("data.txt", "r") as file:
         text = file.read().replace("\n", "")
-    width = 700
-    height = 500
 
     binary_data = text_to_binary(text)
-    image = create_image_with_random_fill(binary_data, width, height)
+    image = create_image_with_random_fill(binary_data, 32)
     image.save("output_image.png")
     image.show()
 
